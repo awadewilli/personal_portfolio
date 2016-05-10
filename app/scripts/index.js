81dedebcccf378dc246cba6824d6a373e75ed6a4
Austin Williams <austinwadewilliams@gmail.com>
2016-05-10 11:46:43 -0400

    most recent changes

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,2 +1,13 @@
 console.log("Hello World!");
 var $ = require('jquery');
+
+$("#about").click(function() {
+    $('html, body').animate({
+        scrollTop: (390)
+    }, 700);
+});
+$("#work").click(function() {
+    $('html, body').animate({
+        scrollTop: (1150)
+    }, 700);
+});
