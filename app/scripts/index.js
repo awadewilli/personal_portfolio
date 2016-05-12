c3b29afa61fa10e9d443d555d50c90584766d171
Austin Williams <austinwadewilliams@gmail.com>
2016-05-12 16:35:36 -0400

    created lightbox for thumbnail images

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,6 +1,8 @@
 console.log("Hello World!");
 var $ = require('jquery');
-
+//////////////////////
+//Scrolling Controls//
+/////////////////////
 $("#about").click(function() {
     $('html, body').animate({
         scrollTop: (390)
@@ -20,4 +22,14 @@ $(".contact-button").click(function() {
     $('html, body').animate({
         scrollTop: ($(document).height())
     }, 800);
+});
+
+$('.port-img').click(function() {
+  $('#lightBox-content').html($(this).clone().addClass('make-large'));
+  $('.lightBox').removeClass('hidden');
+  $('.overlay').removeClass('hidden');
+});
+$('#lightBox-close').click(function() {
+  $('.lightBox').addClass('hidden');
+  $('.overlay').addClass('hidden');
 });
